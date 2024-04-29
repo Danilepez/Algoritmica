a66f0a62fd86882c93de9e0d60f13ad1d4dea3b5
Danilepez <165244790+Danilepez@users.noreply.github.com>
2024-04-29 01:52:53 -0400

    Update numeroCasos.java

--- capitulos/numeroCasos.java
+++ capitulos/numeroCasos.java
@@ -1,22 +1,24 @@
-/* package whatever; // don't place package name! */
+/* package whatever; // No coloques el nombre del paquete */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	static Scanner scan = new Scanner(System.in);
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		int cases = scan.nextInt();
-		for(int i = 0; i < cases; i++) {
-			int a,b,c;
-			a = scan.nextInt();
-			b = scan.nextInt();
-			c = scan.nextInt();
-			System.out.println(a+" "+b+" "+c);
-		}
-	}
+class Ideone {
+    // Declaración de un objeto Scanner para leer desde la entrada estándar
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws java.lang.Exception {
+        int cases = scan.nextInt(); // Leer el número de casos desde la entrada estándar
+
+        // Iterar sobre cada caso
+        for (int i = 0; i < cases; i++) {
+            int a, b, c; // Declarar variables para almacenar los valores de entrada para cada caso
+            a = scan.nextInt(); // Leer el valor de a para el caso actual
+            b = scan.nextInt(); // Leer el valor de b para el caso actual
+            c = scan.nextInt(); // Leer el valor de c para el caso actual
+            System.out.println(a + " " + b + " " + c); // Imprimir los valores de entrada para el caso actual
+        }
+    }
 }
