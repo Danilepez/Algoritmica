e10928b7afa7c3eccea5c9c4cd791b96226511a8
Danilepez <165244790+Danilepez@users.noreply.github.com>
2024-04-29 02:08:01 -0400

    Update ejemploColas.cpp

--- Colas/ejemploColas.cpp
+++ Colas/ejemploColas.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main() {
-    queue<int> cola;
+    queue<int> cola; // Declaración de una cola de enteros
 
     // Enqueue
-    cola.push(1);
-    cola.push(2);
-    cola.push(3);
+    cola.push(1); // Insertar el elemento 1 en la cola
+    cola.push(2); // Insertar el elemento 2 en la cola
+    cola.push(3); // Insertar el elemento 3 en la cola
 
     // Dequeue
-    while (!cola.empty()) {
-        cout << cola.front() << " ";
-        cola.pop();
+    while (!cola.empty()) { // Mientras la cola no esté vacía
+        cout << cola.front() << " "; // Imprimir el primer elemento en la cola
+        cola.pop(); // Eliminar el primer elemento en la cola
     }
 
     return 0;
