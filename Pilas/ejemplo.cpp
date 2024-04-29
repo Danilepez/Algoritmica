3cd2e80c303deca8e96079c5770f738ade537f53
Danilepez <165244790+Danilepez@users.noreply.github.com>
2024-04-29 02:07:40 -0400

    Update ejemplo.cpp

--- Pilas/ejemplo.cpp
+++ Pilas/ejemplo.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 int main() {
-    stack<int> pila;
+    stack<int> pila; // Declaración de una pila de enteros
 
     // Push
-    pila.push(1);
-    pila.push(2);
-    pila.push(3);
+    pila.push(1); // Insertar el elemento 1 en la pila
+    pila.push(2); // Insertar el elemento 2 en la pila
+    pila.push(3); // Insertar el elemento 3 en la pila
 
     // Pop
-    while (!pila.empty()) {
-        cout << pila.top() << " ";
-        pila.pop();
+    while (!pila.empty()) { // Mientras la pila no esté vacía
+        cout << pila.top() << " "; // Imprimir el elemento en la cima de la pila
+        pila.pop(); // Eliminar el elemento en la cima de la pila
     }
 
     return 0;
