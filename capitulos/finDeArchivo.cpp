472c9de5feeed3f819a18fdb08ec083360418391
Danilepez <165244790+Danilepez@users.noreply.github.com>
2024-04-29 01:51:41 -0400

    Update finDeArchivo.cpp

--- capitulos/finDeArchivo.cpp
+++ capitulos/finDeArchivo.cpp
@@ -1,9 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std
+#include<bits/stdc++.h> // Incluir todas las bibliotecas estándar necesarias
+
+using namespace std;
+
 int main() {
-    double costo; 
-    while(cin>>costo) {
-        cout<<costo<<endl;
+    double costo; // Declarar una variable para almacenar el costo
+
+    // Leer continuamente valores de costo hasta que no se puedan leer más
+    while (cin >> costo) {
+        cout << costo << endl; // Imprimir el costo leído
     }
+
     return 0;
 }
