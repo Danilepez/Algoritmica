241c628159e9c13b3db4cc3a61f8c4a51893c311
Danilepez <165244790+Danilepez@users.noreply.github.com>
2024-04-29 01:52:25 -0400

    Update numeroCasos.cpp

--- capitulos/numeroCasos.cpp
+++ capitulos/numeroCasos.cpp
@@ -1,12 +1,17 @@
-#include<bits/stdc++.h>
-using namespace std
+#include<bits/stdc++.h> // Incluir todas las bibliotecas estándar necesarias
+
+using namespace std;
+
 int main() {
-    int cases;
-    cin>>cases;
-    while(cases--) {
-        int a,b,c;
-        cin>>a>>b>>c;
-        cout<<a<<" "<<b<<" "<<c<<endl;
+    int cases; // Declarar una variable para almacenar el número de casos
+    cin >> cases; // Leer el número de casos desde la entrada estándar
+
+    // Iterar sobre cada caso
+    while (cases--) {
+        int a, b, c; // Declarar variables para almacenar los valores de entrada para cada caso
+        cin >> a >> b >> c; // Leer los valores de entrada para el caso actual
+        cout << a << " " << b << " " << c << endl; // Imprimir los valores de entrada para el caso actual
     }
+
     return 0;
 }
