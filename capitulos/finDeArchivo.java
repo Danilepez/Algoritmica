dccecfdb0a4f3d25d7a23e91d00e4a48f46526b7
Danilepez <165244790+Danilepez@users.noreply.github.com>
2024-04-29 01:52:06 -0400

    Update finDeArchivo.java

--- capitulos/finDeArchivo.java
+++ capitulos/finDeArchivo.java
@@ -3,15 +3,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class Ideone
-{
-	public static Scanner scan = new Scanner(System.in);
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		double costo; 
-		while(scan.hasNext()){ 
-			costo = scan.nextDouble();
-			System.out.println(costo);
-		}
-	}
+class Ideone {
+    // Declaración de un objeto Scanner para leer desde la entrada estándar
+    public static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws java.lang.Exception {
+        double costo; // Declaración de una variable para almacenar el costo
+
+        // Leer continuamente valores de costo hasta que no se puedan leer más
+        while (scan.hasNext()) {
+            costo = scan.nextDouble(); // Leer el siguiente valor como un double
+            System.out.println(costo); // Imprimir el valor del costo
+        }
+    }
 }
